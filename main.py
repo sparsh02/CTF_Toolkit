0d1861b47150e80a5fe41c095e3a2e5502a90c22
sparsh02 <54232718+sparsh02@users.noreply.github.com>
2021-05-19 12:11:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    abc = input("Enter name: ")
+    print_hi(abc)
+    print("Your website is open at 0.0.0.0:6969")
+    exec('python E://Submission//Minor_Project-1//CTFtool//manage.py runserver 0.0.0.0:6969 --insecure')
+    
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
